091c7da70edfa4341078f1df8410ae793ea36b48
Jennifer Coy <jennifer.coy@bsu.edu>
2022-11-13 17:02:50 -0500

    Added try-catch to FilmParser.java handle no dataStream data. Improved layout in GUI. Some refatoring of MainGUI.java to make start() method simpler.

--- src/main/java/edu/bsu/cs222/FilmParser.java
+++ src/main/java/edu/bsu/cs222/FilmParser.java
@@ -12,12 +12,16 @@ public class FilmParser {
 
     Film parseFilm(InputStream dataStream) throws IOException {
         // read ONCE from stream, store in JSONArray, then read what we need out of it
-        String jsonResultAll = new String(dataStream.readAllBytes());
-        JSONArray title = JsonPath.read(jsonResultAll, "$..title");
-        JSONArray episode_id = JsonPath.read(jsonResultAll, "$..episode_id");
-        JSONArray opening_crawl = JsonPath.read(jsonResultAll, "$..opening_crawl");
-        JSONArray release_date = JsonPath.read(jsonResultAll, "$..release_date");
-        return new Film(title.get(0).toString(), Integer.parseInt(episode_id.get(0).toString()), opening_crawl.get(0).toString(), release_date.get(0).toString());
+        try {
+            String jsonResultAll = new String(dataStream.readAllBytes());
+            JSONArray title = JsonPath.read(jsonResultAll, "$..title");
+            JSONArray episode_id = JsonPath.read(jsonResultAll, "$..episode_id");
+            JSONArray opening_crawl = JsonPath.read(jsonResultAll, "$..opening_crawl");
+            JSONArray release_date = JsonPath.read(jsonResultAll, "$..release_date");
+            return new Film(title.get(0).toString(), Integer.parseInt(episode_id.get(0).toString()), opening_crawl.get(0).toString(), release_date.get(0).toString());
+        } catch (NullPointerException e) {
+            return new Film("No Response From Server", -1, "", "");
+        }
     }
 
 }
--- src/main/java/edu/bsu/cs222/MainGUI.java
+++ src/main/java/edu/bsu/cs222/MainGUI.java
@@ -18,54 +18,67 @@ import java.io.InputStream;
 public class MainGUI extends Application{
 
     final boolean TESTING = false;   // set to True to use test file, not API
-    Button checkButton;
+    Button checkButton = new Button("Display");
     Text filmTitle = new Text();
     Text filmOpeningCrawl = new Text();
+    Text releaseDate = new Text();
+    Text episodeID = new Text();
     final String[] filmList = { "A New Hope", "The Empire Strikes Back", "Return of the Jedi",
             "The Phantom Menace", "Attack of the Clones", "Revenge of the Sith"};
-    ComboBox<String> selectMovie = new ComboBox<String>(FXCollections.observableArrayList(filmList));
+    ComboBox<String> selectMovie = new ComboBox<>(FXCollections.observableArrayList(filmList));
 
     public static void main(String[] args) {
         launch(args);
     }
     @Override
     public void start(Stage primaryStage) {
-        // Set title, background, etc
         primaryStage.setTitle("Star Wars Trivia");
-        InputStream backgroundImageFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("stars.jpeg");
-        BackgroundImage backgroundImage = new BackgroundImage(new Image(backgroundImageFile),
-                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
-                BackgroundSize.DEFAULT);
-        Background background = new Background(backgroundImage);
-
-        // Set the layout and add background image to grid
+        // Set the layout
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25, 25, 25, 25));
-        grid.setBackground(background);
-        // Formatting
+        // Set title and background
+        InputStream backgroundImageFile = Thread.currentThread().getContextClassLoader().getResourceAsStream("stars.jpeg");
+        if (backgroundImageFile != null) {
+            BackgroundImage backgroundImage = new BackgroundImage(new Image(backgroundImageFile),
+                    BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+                    BackgroundSize.DEFAULT);
+            Background background = new Background(backgroundImage);
+            grid.setBackground(background);
+        }
+        // Formatting text and add to grid, 4 columns wide, Order is (col, row, colspan, rowspan)
+        formatTextFieldsForDisplay();
+        grid.add(selectMovie, 1, 0, 1, 1);
+        grid.add(checkButton, 2, 0, 1, 1);
+        grid.add(filmTitle, 1, 1, 2, 1);
+        grid.add(episodeID, 1, 2, 1, 1);
+        grid.add(releaseDate, 2, 2, 1, 1);
+        grid.add(filmOpeningCrawl, 1, 3, 2, 1);
+        // configure button action
+        checkButton.setOnAction(e -> handleButtonClick());
+        // add grid to the scene and the scene to the stage
+        Scene scene = new Scene(grid, 1000, 1000); // width, height
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void formatTextFieldsForDisplay() {
         filmTitle.setFont(Font.font("Verdana", FontWeight.BOLD, FontPosture.REGULAR, 48));
         filmTitle.setFill(Color.BLACK);
         filmTitle.setStrokeWidth(2);
         filmTitle.setStroke(Color.YELLOW);
         filmTitle.setTextAlignment(TextAlignment.CENTER);
+        releaseDate.setFont(Font.font("Lucida Sans Unicode", FontWeight.NORMAL, FontPosture.ITALIC, 18));
+        releaseDate.setFill(Color.YELLOW);
+        releaseDate.setTextAlignment(TextAlignment.CENTER);
+        episodeID.setFont(Font.font("Lucida Sans Unicode", FontWeight.NORMAL, FontPosture.ITALIC, 18));
+        episodeID.setFill(Color.YELLOW);
+        episodeID.setTextAlignment(TextAlignment.CENTER);
         filmOpeningCrawl.setFont(Font.font("Lucida Sans Unicode", FontWeight.NORMAL, FontPosture.ITALIC, 20));
         filmOpeningCrawl.setFill(Color.WHITE);
         filmOpeningCrawl.setTextAlignment(TextAlignment.CENTER);
-        // Set up contents
-        checkButton = new Button("Use the force, Luke!");
-        grid.add(selectMovie, 0, 0, 1, 1);
-        grid.add(checkButton, 1, 0, 1, 1);
-        grid.add(filmTitle, 0, 1, 3, 1);
-        grid.add(filmOpeningCrawl, 0, 2, 3, 1);
-        // code to handle button action
-        checkButton.setOnAction(e -> handleButtonClick());
-        // add to the stage
-        Scene scene = new Scene(grid, 1000, 1000); // width, height
-        primaryStage.setScene(scene);
-        primaryStage.show();
     }
 
     private void handleButtonClick() {
@@ -89,6 +102,8 @@ public class MainGUI extends Application{
         try {
             film = filmParser.parseFilm(APIresponseStream);
             filmTitle.setText(film.getTitle());
+            releaseDate.setText(film.getRelease_date());
+            episodeID.setText("Episode " + film.getEpisode_id());
             filmOpeningCrawl.setText(openingCrawlFormatter.format(film));
 
         } catch (IOException e) {
