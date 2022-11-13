9f0173d65cbdf79b37b7b5470ce2178893a1ae48
Jennifer Coy <jennifer.coy@bsu.edu>
2022-11-13 15:18:23 -0500

    Added drop down menu to select movie.  Connect menu to function to extract SWAPI index from title.

--- src/main/java/edu/bsu/cs222/MainGUI.java
+++ src/main/java/edu/bsu/cs222/MainGUI.java
@@ -1,10 +1,12 @@
 package edu.bsu.cs222;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
@@ -15,10 +17,13 @@ import java.io.InputStream;
 
 public class MainGUI extends Application{
 
-    final boolean TESTING = true;   // set to True to use test file, not API
+    final boolean TESTING = false;   // set to True to use test file, not API
     Button checkButton;
     Text filmTitle = new Text();
     Text filmOpeningCrawl = new Text();
+    final String[] filmList = { "A New Hope", "The Empire Strikes Back", "Return of the Jedi",
+            "The Phantom Menace", "Attack of the Clones", "Revenge of the Sith"};
+    ComboBox<String> selectMovie = new ComboBox<String>(FXCollections.observableArrayList(filmList));
 
     public static void main(String[] args) {
         launch(args);
@@ -32,6 +37,7 @@ public class MainGUI extends Application{
                 BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
                 BackgroundSize.DEFAULT);
         Background background = new Background(backgroundImage);
+
         // Set the layout and add background image to grid
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
@@ -50,6 +56,7 @@ public class MainGUI extends Application{
         filmOpeningCrawl.setTextAlignment(TextAlignment.CENTER);
         // Set up contents
         checkButton = new Button("Use the force, Luke!");
+        grid.add(selectMovie, 0, 0, 1, 1);
         grid.add(checkButton, 1, 0, 1, 1);
         grid.add(filmTitle, 0, 1, 3, 1);
         grid.add(filmOpeningCrawl, 0, 2, 3, 1);
@@ -64,17 +71,18 @@ public class MainGUI extends Application{
     private void handleButtonClick() {
         checkButton.setDisable(true);
         String output;
-        String targetPage = "films/1";
         Film film;
         FilmParser filmParser = new FilmParser();
         OpeningCrawlFormatter openingCrawlFormatter = new OpeningCrawlFormatter();
+        // set up request
+        String targetPage = "films/" + getIndexFromTitle(selectMovie.getValue());
         // connect to API
         InputStream APIresponseStream;
         if (TESTING == false) {
             SWAPIconnect SWAPIconnection = new SWAPIconnect(targetPage);
             APIresponseStream = SWAPIconnection.connect();
         } else {
-            String filename = String.format("films-%d.json", 1);
+            String filename = String.format("films-%d.json", getIndexFromTitle(selectMovie.getValue()));
             APIresponseStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(filename);
         }
         // process response and display
@@ -88,5 +96,15 @@ public class MainGUI extends Application{
             filmOpeningCrawl.setText(output);
         }
         checkButton.setDisable(false);
+    }
+
+    private int getIndexFromTitle(String title) {
+        for (int i=0; i<filmList.length; i++) {
+            if (title.equals(filmList[i])) {
+                return (i+1); // A New Hope is located at [0], but has SWAPI index of 1
+            }
+        }
+        // did not find film in list
+        return 0;
     }
 }
